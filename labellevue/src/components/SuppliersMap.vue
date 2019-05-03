6cf94aac47ec991c50422bbc321bffbd17e26538
Tom <thomas.bozon@campu_numerique.fr>
2019-05-03 16:17:15 +0200

    exos 8 en cours les promess

--- labellevue/src/components/SuppliersMap.vue
+++ labellevue/src/components/SuppliersMap.vue
@@ -1,17 +1,38 @@
 
 <template>
   <div class="container mb-5">
-    
-      <div>
-        <h1>Carte des fournisseurs</h1>
-        <gmap-map :center="{lat:10, lng:10}" :zoom="7" style="width: 100%; height: 500px"></gmap-map>
-      </div>
-    
+    <div>
+      <h1>Carte des fournisseurs</h1>
+      <gmap-map :center="{lat:46, lng:6}" :zoom="7" style="width: 100%; height: 500px">
+        <GmapMarker
+          v-for="(m, index) in markers"
+          :key="index"
+          :position="{lat:m.latitude, lng:m.longitude}"
+          @click="true "
+        />
+      </gmap-map>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "suppliersmap"
+  name: "suppliersmap",
+  data() {
+    return {
+      markers: [
+        {
+          id: 1,
+          latitude: 45.859779,
+          longitude: 6.195746
+        },
+        {
+          id: 2,
+          latitude: 45.866360,
+          longitude: 6.139886
+        }
+      ]
+    };
+  }
 };
 </script>
